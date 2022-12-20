1ea433856b7806107b788c5c22fb741f112b1a9a
thelebaron <thelebaron@gmail.com>
2022-12-19 21:52:35 -0500

    add transformV2 conditional code

---
+++ Runtime/TransformBaking/ResetTransformsBakingSystem.cs
@@ -0,0 +1,27 @@
+﻿#if !ENABLE_TRANSFORM_V1
+
+using Unity.Entities;
+using Unity.Transforms;
+
+namespace Unity.Rendering
+{
+    // TODO: investigate if its a unity bug that causes the incorrect transform data at start
+    [WorldSystemFilter(WorldSystemFilterFlags.BakingSystem)]
+    [UpdateInGroup(typeof(PostBakingSystemGroup))]
+    [RequireMatchingQueriesForUpdate]
+    public partial class ResetTransformsBakingSystem : SystemBase
+    {
+        protected override void OnUpdate()
+        {
+            Entities
+                .WithAll<DeformedEntity>()
+                .ForEach((Entity entity, ref LocalTransform localTransform) =>
+                {
+                    localTransform = LocalTransform.Identity;
+                }).WithEntityQueryOptions(EntityQueryOptions.IncludeDisabledEntities | EntityQueryOptions.IncludePrefab).WithoutBurst()
+                .WithStructuralChanges().Run();
+        }
+    }
+}
+
+#endif
